4ba8c2d268ce098b28eda61540b924cb8763355f
Furqan <furqanahmedmj@gmail.com>
2023-03-29 21:31:56 +0530

    Navbar (Furqan)

--- constuctweek/src/App.js
+++ constuctweek/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import NavMenus from './NavMenus';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <NavMenus/>
     </div>
   );
 }
