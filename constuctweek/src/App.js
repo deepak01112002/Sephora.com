4c536ca33d7a47b441a8d9e990f151d535872b4b
Gurmeet Singh <gurmeetsingh6175@gmail.com>
2023-03-29 22:16:14 +0530

    Admin Page Added By Gurmeet

--- constuctweek/src/App.js
+++ constuctweek/src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Admin from "./Gurmeet/Admin";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <Admin />;
 }
 
 export default App;
