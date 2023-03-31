9fd2bf8500bf5365e2d9d26b2fade5a23e0a2c64
Deepak Pandey <deepak8574757609@gmail.com>
2023-03-31 13:43:57 +0530

    login link

--- constuctweek/src/Components/MainRoutes.jsx
+++ constuctweek/src/Components/MainRoutes.jsx
@@ -4,14 +4,19 @@ import Admin from '../Gurmeet/Admin'
 import Product from './Product'
 import SinglePage from './SinglePage'
 import Home from '../Pages/Home'
+import Cart from '../Sanjeet/Cart'
+import Login from './Login'
+
 function MainRoutes() {
   return (
     <div>
     <Routes>
         <Route path={"/"} element={<Home/>}></Route>
+       
         <Route path='/admin' element={<Admin/>}></Route>
         <Route path='/Makeup' element={<Product/>}></Route>
         <Route path="/admin" element={<Admin/>}></Route>
+        <Route path='/cart' element={<Cart/>}></Route>
         <Route path="/Makeup/:id" element={<SinglePage/>}></Route>
         
     </Routes>        
