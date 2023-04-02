0fd5f970e1dc98b28d130654e62e37b7a374382f
NitinGautam09 <hinitingautam@gmail.com>
2023-04-02 16:11:26 +0530

    day-5 second half

--- constuctweek/src/Navbar.jsx
+++ constuctweek/src/Navbar.jsx
@@ -8,6 +8,7 @@ import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import { useNavigate } from 'react-router-dom';
 import { FaEnvelope, FaLock } from 'react-icons/fa';
+import { Col, Image, Row } from 'react-bootstrap';
 const Navbar = () => {
 
    const [showSignUpModal, setShowSignUpModal] = useState(false);
@@ -100,7 +101,6 @@ const Navbar = () => {
     </div>
     
   );
-
 };
 
 export default Navbar;
@@ -109,38 +109,130 @@ export default Navbar;
 function SignUpModal({ show, onHide }) {
   const [isSignedUp, setIsSignedUp] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+  const [selectedMonth, setSelectedMonth] = useState('');
+  const [zipCode, setZipCode] = useState('');
 
-  
 
-  
 
+
+
+  const [showSignUpModal, setShowSignUpModal] = useState(false);
+const [modalVisible, setModalVisible] = useState(false);
+
+  const handleFirstNameChange = (event) => setFirstName(event.target.value); //
+  const handleLastNameChange = (event) => setLastName(event.target.value);
   const handleEmailChange = (event) => setEmail(event.target.value);
-  const handlePasswordChange = (event) => setPassword(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);//
+  const handlePhoneNumberChange = (event) => setPhoneNumber(event.target.value);
+  const handleDateChange = (event) => setSelectedDate(event.target.value);
+  const handleMonthChange = (event) => setSelectedMonth(event.target.value);
+  const handleZipCodeChange = (event) => setZipCode(event.target.value);
+
+
+  const handleShowSignUpModal = () => {
+    setShowSignUpModal(true);
+    setModalVisible(true);
+  };
+  const handleHideSignUpModal = () => {
+    onHide();
+    setIsSignedUp(false);
+    setShowSignUpModal(false);
+    setModalVisible(false);
+  };
+
+  const obj={
+    firstName:firstName,
+    lastName:lastName,
+    email:email,
+    password:password,
+    phoneNumber:phoneNumber,
+    date:selectedDate,
+    Month:selectedMonth,
+    zipCode:zipCode
+  }
+
+
+  // write the dispatch code here in handleSignUpButtonClick
+
 
   const handleSignUpButtonClick = () => {
     // TODO: Implement logic to handle sign up
     setIsSignedUp(true);
     setShowLoginModal(true);
+
+    console.log(obj);
+
+    
+    setPhoneNumber("");
+    setSelectedDate("");
+    setSelectedMonth("");
+    setZipCode("");
+    setFirstName("");
+    setLastName("");
+    setEmail("") ;
+    setPassword("");
+
+    
   };
 
-  const handleHideSignUpModal = () => {
-    onHide();
-    setIsSignedUp(false);
-  };
+ 
 
   const handleShowLoginModal = () => setShowLoginModal(true);
 
   const handleHideLoginModal = () => setShowLoginModal(false);
 
+
+  useEffect(() => {
+  if (modalVisible) {
+    document.body.classList.add("modal-open");
+  } else {
+    document.body.classList.remove("modal-open");
+  }
+}, [modalVisible]);
+
   return (
     <Modal show={show} onHide={handleHideSignUpModal}>
       <Modal.Header closeButton>
-        <Modal.Title>Sign Up</Modal.Title>
+        <Modal.Title style={{testAlign:"center"}}>Create an Account</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
+        <Form.Group>
+        <Image src="https://www.sephora.com/img/ufe/bi/logo-beauty-insider.svg"width="50%"/>
+        <Form.Text>Join the Beauty Insider loyalty program. Earn points, get FREE standard shipping, redeem rewards, and more.</Form.Text>
+       
+        </Form.Group>
+       
+          <Row>
+            <Col>
+              <Form.Group controlId="formBasicFirstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter first name"
+                  value={firstName}
+                  onChange={handleFirstNameChange}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="formBasicLastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter last name"
+                  value={lastName}
+                  onChange={handleLastNameChange}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control
@@ -149,18 +241,83 @@ function SignUpModal({ show, onHide }) {
               value={email}
               onChange={handleEmailChange}
             />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
-              placeholder="Password"
+              placeholder="Enter password (6-12 characters)"
               value={password}
               onChange={handlePasswordChange}
             />
           </Form.Group>
-          <Button variant="primary" onClick={handleSignUpButtonClick}>
-            Sign Up
+          <Form.Group controlId="formBasicPhoneNumber">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter phone number"
+              value={phoneNumber}
+              onChange={handlePhoneNumberChange}
+            />
+          </Form.Group>
+
+          <Form.Group>
+        <Form.Control plaintext readOnly defaultValue=" Enter your birthdate to receive a free gift every year." />
+        </Form.Group>
+        <Row>
+            <Col>
+              <Form.Group >
+              <select value={selectedMonth} onChange={handleMonthChange} style={{ height: "40px", marginBottom: "10px" }}>
+        <option value="">Select month</option>
+        <option value="Jan">January</option>
+        <option value="Feb">February</option>
+        <option value="Mar">March</option>
+        <option value="Apr">April</option>
+        <option value="May">May</option>
+        <option value="Jun">June</option>
+        <option value="Jul">July</option>
+        <option value="Aug">August</option>
+        <option value="Sep">September</option>
+        <option value="Oct">October</option>
+        <option value="Nov">November</option>
+        <option value="Dec">December</option>
+      </select>
+               
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group >
+              <select value={selectedDate} onChange={handleDateChange} style={{ height: "40px", marginBottom: "10px" , border:"1px"}}>
+        <option value="">Select date</option>
+        {[...Array(31)].map((_, index) => (
+          <option key={index} value={index + 1}>
+            {index + 1}
+          </option>
+        ))}
+      </select>
+
+              </Form.Group>
+               
+            </Col>
+        
+          </Row>
+
+          <Form.Group controlId="formBasicZipCode">
+            <Form.Label>Zip Code</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="ZIP Code (to hear about store event near you)"
+              value={zipCode}
+             
+              onChange={handleZipCodeChange}
+            />
+          </Form.Group>
+
+          <Button variant="primary" style={{marginTop:"30px", backgroundColor:"black", color:"white"}} onClick={handleSignUpButtonClick}>
+          CREATE ACCOUNT
           </Button>
         </Form>
       </Modal.Body>
@@ -179,6 +336,7 @@ function SignUpModal({ show, onHide }) {
   );
 }
 
+
 function LoginModal({ show, onHide }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [email, setEmail] = useState('');
@@ -191,9 +349,22 @@ function LoginModal({ show, onHide }) {
   const handlePasswordChange = (event) => setPassword(event.target.value);
 
   const handleLoginButtonClick = () => {
+    console.log(email,password)
+    
+    
+    if (email === 'admin@mail.com') {
+      window.location.href = '/admin';
+    }else
+    {
+      setIsLoggedIn(true);
+      window.location.reload();
+    }
+   
     // TODO: Implement logic to handle login
-    setIsLoggedIn(true);
+   
+   
     navigate('');
+    
    
   };
 
@@ -209,6 +380,10 @@ function LoginModal({ show, onHide }) {
       </Modal.Header>
       <Modal.Body>
         <Form>
+        <Form.Group>
+        <Image src="https://www.sephora.com/img/ufe/bi/logo-beauty-insider.svg"width="30%"/>
+        <Form.Text>Sign in or create an account to enjoy FREE standard shipping on all orders.</Form.Text>
+        </Form.Group>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control
@@ -227,7 +402,7 @@ function LoginModal({ show, onHide }) {
         onChange={handlePasswordChange}
       />
     </Form.Group>
-    <Button variant="primary" onClick={handleLoginButtonClick}>
+    <Button variant="primary" onClick={handleLoginButtonClick} style={{marginTop:"30px", backgroundColor:"black", color:"white"}}>
     Log In
     </Button>
     </Form>
